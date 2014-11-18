dff880c0a926d48c7a7ac430311ba4a82d265768
purplefox <timvolpe@gmail.com>
2014-11-18 07:27:24 +0000

    tweaked settings again

--- src/test/java/io/vertx/test/core/HazelcastTestProperties.java
+++ src/test/java/io/vertx/test/core/HazelcastTestProperties.java
@@ -21,7 +21,7 @@ package io.vertx.test.core;
  */
 public class HazelcastTestProperties {
   public static void setProperties() {
-    System.setProperty("hazelcast.wait.seconds.before.join", "0");
+//    System.setProperty("hazelcast.wait.seconds.before.join", "0");
     System.setProperty("hazelcast.local.localAddress", "127.0.0.1");
 //    System.setProperty("hazelcast.icmp.enabled","true");
 //    System.setProperty("hazelcast.icmp.timeout","5000");
