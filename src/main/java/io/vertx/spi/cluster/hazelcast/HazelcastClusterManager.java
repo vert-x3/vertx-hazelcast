82c4a3f5be28c787079975edff03a4a057c5b297
Thomas Segismont <tsegismo@redhat.com>
2020-01-29 11:12:31 +0100

    Added a dedicated executor to release locks (#122)
    This avoids worker starvation when lock concurrency is greater than worker pool size.
    
    See eclipse-vertx/vert.x#3126

--- src/main/java/io/vertx/spi/cluster/hazelcast/HazelcastClusterManager.java
+++ src/main/java/io/vertx/spi/cluster/hazelcast/HazelcastClusterManager.java
@@ -36,6 +36,8 @@ import io.vertx.spi.cluster.hazelcast.impl.HazelcastInternalAsyncCounter;
 import io.vertx.spi.cluster.hazelcast.impl.HazelcastInternalAsyncMap;
 
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -82,6 +84,8 @@ public class HazelcastClusterManager implements ClusterManager, MembershipListen
 
   private Config conf;
 
+  private ExecutorService lockReleaseExec;
+
   /**
    * Constructor - gets config from classpath
    */
@@ -112,6 +116,8 @@ public class HazelcastClusterManager implements ClusterManager, MembershipListen
     vertx.executeBlocking(fut -> {
       if (!active) {
         active = true;
+
+        lockReleaseExec = Executors.newCachedThreadPool(r -> new Thread(r, "vertx-hazelcast-service-release-lock-thread"));
 
         // The hazelcast instance has not been passed using the constructor.
         if (!customHazelcastCluster) {
@@ -238,6 +244,7 @@ public class HazelcastClusterManager implements ClusterManager, MembershipListen
         if (active) {
           try {
             active = false;
+            lockReleaseExec.shutdown();
             boolean left = hazelcast.getCluster().removeMembershipListener(membershipListenerId);
             if (!left) {
               log.warn("No membership listener");
@@ -454,10 +461,7 @@ public class HazelcastClusterManager implements ClusterManager, MembershipListen
     @Override
     public void release() {
       if (released.compareAndSet(false, true)) {
-        vertx.executeBlocking(future -> {
-          semaphore.release();
-          future.complete();
-        }, false, null);
+        lockReleaseExec.execute(semaphore::release);
       }
     }
   }
